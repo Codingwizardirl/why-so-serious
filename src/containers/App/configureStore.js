74ab0c55060837b12bc5d03a3a6ab001b8dc562b
Codingwizardirl <pavel.r.georgiev@hotmail.com>
2017-05-26 23:15:40 +0100

    Added redux-thunk and react-router

--- src/containers/App/configureStore.js
+++ src/containers/App/configureStore.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers'
 
 
@@ -7,6 +8,7 @@ export default function configureStore(preloadedState) {
     rootReducer,
     preloadedState,
     applyMiddleware(
+      thunkMiddleware,
     )
   )
 }
