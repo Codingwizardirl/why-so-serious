5aafb331c045a5f9dc3f8a9b06fde4e02cf528c1
Codingwizardirl <pavel.r.georgiev@hotmail.com>
2017-06-04 16:00:08 +0100

    Rebranding hahah

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux'
 import configureStore from './configureStore'
 import Main from '../Main';
-import logo from '../../logo.svg';
 import './App.css';
 
 const store = configureStore()
@@ -17,8 +16,6 @@ class App extends Component {
       <Provider store={store}>
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Why so serious?</h2>
         </div>
         <div className="input-container">
           <Main />
